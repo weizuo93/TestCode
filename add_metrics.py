3d6992fc192af93ec09d04245131efd7bac8bfb8
weizuo <weizuo@xiaomi.com>
2020-12-08 21:17:30 +0800

    add metrics for grafana

--- add_metrics.py
+++ add_metrics.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import copy
+import time
 import uuid
 
 
@@ -13,21 +14,33 @@ def request_metrics(host, port):
         return response.text
 
 
-def parase_metrics(str):
+def parse_metrics(str):
     lines = str.split("\n")
-    metrics = {}
+    metrics = []
     for line in lines:
         if line.startswith("#"):
             continue
         else:
-            item = line.strip().split()
+            item = line.strip().split()                        # 使用“ ”分割metric和值
             if len(item) == 2:
-                metrics[item[0]] = item[1]
+                # print(item[0])
+                metric_item = dict()
+                metric_params = item[0].strip("}").split("{")   # 使用"{"分割metric与其后面{}中的参数列表，比如：doris_be_engine_requests_total{status="failed",type="publish"}
+                metric = metric_params[0]
+                metric_item["name"] = metric
+                if len(metric_params) == 2:
+                    params = metric_params[1].strip().split(",")  # 使用“,”分割参数列表中的每一个参数
+                    for param in params:
+                        kv = param.strip().split("=")             # 使用“=”分割参数列表中的每一个参数的key和value
+                        k = kv[0]
+                        v = kv[1].strip("\"")
+                        metric_item[k] = v
+                metrics.append(metric_item)
     return metrics
 
 
 def create_panel():
-    panel = {}
+    panel = dict()
     panel["aliasColors"] = {}
     panel["bars"] = False
     panel["dashLength"] = 10
@@ -35,9 +48,10 @@ def create_panel():
     panel["datasource"] = "default"
     panel["description"] = ""
     panel["fill"] = 0
+    panel["fillGradient"] = 0
     panel["gridPos"] = {}
     panel["id"] = 0
-    panel["legend"] = {"alignAsTable": True, "avg": False, "current": True, "max": False, "min": False,
+    panel["legend"] = {"alignAsTable": True, "avg": False, "current": False, "max": False, "min": False,
                        "rightSide": False, "show": True, "total": False, "values": False}
     panel["lines"] = True
     panel["linewidth"] = 1
@@ -58,49 +72,47 @@ def create_panel():
     panel["title"] = ""
     panel["tooltip"] = {"shared": True, "sort": 0, "value_type": "individual"}
     panel["type"] = "graph"
-    panel["xaxis"] = {"buckets": None, "mode": "time", "name": None, "show": None, "values": []}
-    panel["yaxes"] = [{"decimals": None, "format": "short", "label": None, "logBase": 1, "max": None, "min": "0",
+    panel["xaxis"] = {"buckets": None, "mode": "time", "name": None, "show": True, "values": []}
+    panel["yaxes"] = [{"decimals": None, "format": "short", "label": None, "logBase": 1, "max": None, "min": None,
                        "show": True},
-                      {"decimals": 0, "format": "short", "label": None, "logBase": 1, "max": None, "min": None,
-                       "show": True}]
+                      {"decimals": None, "format": "short", "label": None, "logBase": 1, "max": None, "min": None,
+                       "show": False}]
     panel["yaxis"] = {"align": False, "alignLevel": None}
     return panel
 
 
-def parase_grafana_json_file(file_name, metrics):
+def parse_grafana_json_file(file_name, metrics):
     with open(file_name, "r") as file:
-        dashboard = json.load(file)
+        dashboard = json.load(file)                     # 加载模板文件
         # print(json_str)
-        dashboard["title"] = "generate-dashboard-test"
+        dashboard["title"] = "dashboard-test"
         dashboard["uid"] = str(uuid.uuid1())
         rows = dashboard["panels"]
-        # for row in rows:
-        #     print(row["title"])
 
-        row = {}
+        row = dict()
         row["collapsed"] = True
         row["datasource"] = None
-        row["id"] = 76
-        gridPos = {}
+        row["id"] = 76                                  # 基于模板文件修改
+        gridPos = dict()
         gridPos["h"] = 1
         gridPos["w"] = 24
         gridPos["x"] = 0
-        gridPos["y"] = 29
+        gridPos["y"] = 29                                # 基于模板文件修改
         row["gridPos"] = gridPos
-        row["title"] = "New Metrics Panel"
+        row["title"] = "BE Other Metrics"
         row["type"] = "row"
         row["panels"] = []
         i = 0
-        for key in metrics:
+        for metric in metrics:
             mod = i % 3
             div = i // 3
-            met = str(key)
-            item = met.strip().split("{")
-            print(item)
+            # met = str(key)
+            # item = met.strip().split("{")
+            # print(key)
 
             panel = copy.deepcopy(create_panel())
-            panel["id"] = 100 + i
-            panel["title"] = "[$cluster_name] " + str(item[0])
+            panel["id"] = 200 + i                          # 基于模板文件修改
+            panel["title"] = "[$cluster_name] " + str(metric["name"])
 
             panel["gridPos"]["h"] = 6
             panel["gridPos"]["w"] = 8
@@ -108,11 +120,15 @@ def parase_grafana_json_file(file_name, metrics):
             panel["gridPos"]["y"] = row["gridPos"]["y"] + div * 6 + 1
 
             panel_targets = []
-            target = {}
-            target["expr"] = str(item[0]) + "{job=\"$cluster_name\"}"
+            target = dict()
+            s = ""
+            for key in metric:
+                if key != "name" and key != "path":
+                    s = s + ", " + str(key) + "=\"" + str(metric[key]) + "\""
+            target["expr"] = str(metric["name"]) + "{job=\"$cluster_name\"" + s + "}"
             target["format"] = "time_series"
             target["intervalFactor"] = 1
-            target["legendFormat"] = "{{instance}}"
+            # target["legendFormat"] = "{{instance}}"
             target["refId"] = "A"
             panel_targets.append(target)
             panel["targets"] = copy.deepcopy(panel_targets)
@@ -132,10 +148,8 @@ def parase_grafana_json_file(file_name, metrics):
 
 if __name__ == '__main__':
     metrics = request_metrics("c3-hadoop-doris-tst-st01.bj", "8040")
+    metrics = parse_metrics(metrics)
     # print(metrics)
-    metrics = parase_metrics(metrics)
-    # for key in metrics:
-    #     print(str(key) + " : " + str(metrics[key]))
 
-    parase_grafana_json_file("./grafana_json.txt", metrics)
+    parse_grafana_json_file("./grafana_json.txt", metrics)
 
