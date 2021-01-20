a25bb004d2bb2572fefdbc8e1c41ad09c619c9b2
weizuo <weizuo@xiaomi.com>
2021-01-20 18:34:47 +0800

    tablet migration strategy for disk load balance

--- TabletAntibalance.py
+++ TabletAntibalance.py
@@ -2,6 +2,7 @@ import requests
 import json
 import copy
 import time
+import random
 
 
 # 获取be上所有partition的信息
@@ -54,58 +55,23 @@ def get_tablet_distribution(host, port, partition_id):
 
 # 根据获取的单个partition下tablet在各个磁盘之间的分布信息进行迁移策略规划
 def migration_plan(tablet_distribution):
-    tablet_num = 0
-    for i in tablet_distribution:
-        tablet_num = tablet_num + len(tablet_distribution[i])
+    disks = []
+    for disk in tablet_distribution:
+        disks.append(disk)
 
-    average = tablet_num / len(tablet_distribution)
-    print('\ntablet分布均值 : {}\n'.format(average))
-
-    out_disk = {}
-    in_disk = {}
-    for i in tablet_distribution:
-        if len(tablet_distribution[i]) > average:
-            out_disk[i] = int(len(tablet_distribution[i]) - average)
-        elif len(tablet_distribution[i]) < average:
-            if int(average - len(tablet_distribution[i])) == (average - len(tablet_distribution[i])):
-                in_disk[i] = int(average - len(tablet_distribution[i]))
-            else:
-                in_disk[i] = int(average - len(tablet_distribution[i])) + 1
-
-    print('磁盘需要迁出的tablet数量:')
-    for i in out_disk:
-        print('{} : {}'.format(i, out_disk[i]))
-
-    print('磁盘可以迁入的tablet数量:')
-    for i in in_disk:
-        print('{} : {}'.format(i, in_disk[i]))
-
-    # print('迁移规划：')
     migration_items = []
-    for i in out_disk:
-        for j in in_disk:
-            if out_disk[i] <= in_disk[j]:
-                for k in range(out_disk[i]):
-                    dict = copy.deepcopy(tablet_distribution[i][k])
-                    tablet_distribution[i].pop(k)
-                    tablet_distribution[j].append(dict)
-                    dict['from_disk'] = i
-                    dict['to_disk'] = j
-                    migration_items.append(dict)
-                in_disk[j] = in_disk[j] - out_disk[i]
-                out_disk[i] = 0
-                break
-            else:
-                for k in range(in_disk[j]):
-                    dict = copy.deepcopy(tablet_distribution[i][k])
-                    tablet_distribution[i].pop(k)
-                    tablet_distribution[j].append(dict)
-                    dict['from_disk'] = i
-                    dict['to_disk'] = j
-                    migration_items.append(dict)
-                out_disk[i] = out_disk[i] - in_disk[j]
-                in_disk[j] = 0
-                continue
+    for disk in tablet_distribution:
+        tablet_list = tablet_distribution[disk]
+        for i in range(len(tablet_list)):
+            migration_task = dict()
+            migration_task['tablet_id'] = tablet_list[i]["tablet_id"]
+            migration_task['schema_hash'] = tablet_list[i]["schema_hash"]
+            migration_task['tablet_size'] = tablet_list[i]["tablet_size"]
+            migration_task['from_disk'] = disk
+            id = random.randint(0, len(disks) - 1)
+            migration_task['to_disk'] = disks[id]
+            migration_items.append(migration_task)
+
     return migration_items
 
 
@@ -142,8 +108,8 @@ def query_tablet_migration_status(host, port, tablet_id, schema_hash):
 
 
 if __name__ == '__main__':
-    be_host = 'c3-hadoop-doris-tst-st01.bj'
-    webserver_port = '28040'
+    be_host = ''
+    webserver_port = ''
     partitions_info = get_all_partitions_info(be_host, webserver_port)
     # for partition_id in partitions_info:
     #     print(str(partition_id))
@@ -155,7 +121,7 @@ if __name__ == '__main__':
     for partition_id in partitions_info:
         # 执行单个partition下的tablet在各个磁盘之间的re-balance
         print('\n**********************************************************************************')
-        print('              Tablet Re-balance For Partition: ' + str(partition_id))
+        print('              Tablet Anti-balance For Partition: ' + str(partition_id))
         print('**********************************************************************************\n')
         tablet_distribution_backend = get_tablet_distribution(be_host, webserver_port, partition_id)  # 获取当前partition下的tablet在各个磁盘之间的分布
         for par_id in tablet_distribution_backend:
@@ -170,26 +136,20 @@ if __name__ == '__main__':
                 # print('{} : {}'.format(i, tablet_distribution[i]))
 
             migration_items = migration_plan(tablet_distribution)  # 迁移策略规划
-            print('submit migration tasks:')
+            print('migration tasks:')
             for i in range(len(migration_items)):
                 print('---------------------------------------------------------------------------------------------------')
                 print(migration_items[i])
                 submit_tablet_migration_task(be_host, webserver_port, str(migration_items[i]["tablet_id"]), str(migration_items[i]["schema_hash"]), str(migration_items[i]["to_disk"]))  # 提交单个tablet的磁盘间迁移任务
-                # if not status:
-                #     print('There is something wrong when submit tablet migration task')
-                #     break
-                print('---------------------------------------------------------------------------------------------------')
-
-            print('query migration status:')
-            for i in range(len(migration_items)):
-                print('---------------------------------------------------------------------------------------------------')
-                print(migration_items[i])
                 while True:
                     status = query_tablet_migration_status(be_host, webserver_port, str(migration_items[i]["tablet_id"]), str(migration_items[i]["schema_hash"]))  # 查询单个tablet的磁盘间迁移状态
                     if status:
                         break
                     else:
                         time.sleep(1)  # sleep 1秒
+                # if not status:
+                #     print('There is something wrong when submit tablet migration task')
+                #     break
                 print('---------------------------------------------------------------------------------------------------')
 
         tablet_distribution_backend = get_tablet_distribution(be_host, webserver_port, partition_id)
