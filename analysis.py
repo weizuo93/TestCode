5af56ebb5d58552ef3ab796607d10ad9ef217aa0
weizuo <weizuo@xiaomi.com>
2021-03-29 10:31:23 +0800

    pprof catch memory and analysis

--- analysis.py
+++ analysis.py
@@ -1,6 +1,7 @@
 import os
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
+from matplotlib.widgets import Cursor
 
 
 def analysis(path, prefix):
@@ -24,18 +25,27 @@ def analysis(path, prefix):
                 if len(li) == 3:
                     print("mem: " + li[1] + " " + li[2] + ", file: " + file)
                     mem_malloc.append(float(li[1]))
-                    time.append(file.split(prefix)[1])
+                    time.append(file.split(prefix)[-1])
                 else:
                     print("Error." + line)
             f.close()
 
     '''使用折线图呈现'''
+    x = range(len(time))
+    y = mem_malloc
+
     fig, ax = plt.subplots(1, 1)
-    plt.plot(time, mem_malloc, 'b', label='memory')  # 图表中第2条折线的配置
+    plt.plot(x, y, 'b', label='memory')  # 图表中折线的配置
     plt.xlabel('Time')       # x轴标签
-    tick_spacing = len(files) / 15  # x轴文字的显示间隔（图标中一共显示15个x轴文字）
-    ax.xaxis.set_major_locator(ticker.MultipleLocator(tick_spacing))
-    plt.xticks(rotation=15)  # x轴文字逆时针旋转15°
+    plt.xticks(x, time, rotation=15)
+    # plt.xticks(rotation=90)  # x轴文字逆时针旋转15°
+    # tick_spacing = len(files) / 20  # x轴文字的显示间隔（图标中一共显示15个x轴文字 len(files) / 15）
+    # ax.xaxis.set_major_locator(ticker.MultipleLocator(tick_spacing))
+    ax.xaxis.set_ticks([])
+    # ax.yaxis.set_major_locator(ticker.MultipleLocator(tick_spacing))
+    ax.format_coord = lambda _x, _y: 'x={}, y={:g}'.format(time[int(_x)], _y)
+    curse = Cursor(ax, useblit=True, color='r', linewidth=1)
+
     plt.ylabel('Mem/MB')     # y轴标签
     plt.title('Memory Allocation')  # 图表标题
     plt.legend()  # 设置标签
@@ -43,4 +53,4 @@ def analysis(path, prefix):
 
 
 if __name__ == '__main__':
-    analysis("data_sgp2_be20", "sgp2_be20.mem.")
+    analysis("data_sgp2_be05/pprof", "mem.")
