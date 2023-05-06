a895ef51600f355cc1ca315f98e8c7e01a994f5f
weizuo <weizuo@xiaomi.com>
2023-05-06 18:17:04 +0800

    simple C++ thread pool implement

--- main.cpp
+++ main.cpp
@@ -9,18 +9,20 @@ void task(int i) {
     std::this_thread::sleep_for(std::chrono::seconds((rand() % 5) + 1));
 }
 
+void monitor(ThreadPool& thread_pool, bool& monitor_flag) {
+    while (monitor_flag) {
+        std::cout << "thread_num : " << thread_pool.thread_num() << ", task_queue_size : " << thread_pool.task_queue_size() << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    }
+}
+
 int main() {
 
     ThreadPool thread_pool(5, 20, 10000);
     thread_pool.init();
 
     bool monitor_flag = true;
-    std::thread monitor_thread([&thread_pool, &monitor_flag]() {
-       while (monitor_flag) {
-           std::cout << "thread_num : " << thread_pool.thread_num() << ", task_queue_size : " << thread_pool.task_queue_size() << std::endl;
-           std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-       }
-    });
+    std::thread monitor_thread(monitor, std::ref(thread_pool), std::ref(monitor_flag));
 
     bool producer_flag = true;
     std::thread producer_thread([&thread_pool, &producer_flag](){
